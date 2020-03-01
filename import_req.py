9f503be3c143ef192cb75ccf9e4e7a688d5091bf
hypnoticpenguin <chankevin234@gmail.com>
2020-03-01 17:48:34 -0500

    Added code to import

--- import_req.py
+++ import_req.py
@@ -1,9 +1,25 @@
 import requests
-import pprint
+from bs4 import BeautifulSoup
 
-URL = 'https://ca.indeed.com/jobs?q=software+developer&l=Toronto%2C+ON'
-page = requests.get(URL)
-pp = pprint.PrettyPrinter(indent=4)
+URL = 'https://ca.indeed.com/jobs?q=software+developer&l=Toronto%2C+ON' #url
+page = requests.get(URL).text #page object
 
-print(page)
-print(pp)
+soup = BeautifulSoup(requests.get(URL).text, 'html.parser') #instantiate bsoup object
+
+results = soup.find(id='ResultsContainer')
+#print(results.prettify()) 
+
+#select items in <selection> within 
+
+job_elems = results.find_all('section', class_='card-content')
+
+for job_elem in job_elems:
+    #print(job_items, end='\n'*2) --> print all bsoup objects
+    title_elem = job_elem.find('h2', class_='title') #job_title elements
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+
+    print(title_elem.text)
+    print(company_elem.text) 
+    print(location_elem.text)
+    print()
